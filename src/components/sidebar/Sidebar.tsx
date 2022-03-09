9d7e86e97636e5f9369676d382af17fe2b63ca2b
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-09 21:52:30 +0330

    feat: :art: sidebar structure and styles

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -17,16 +17,25 @@ function Sidebar() {
           <Link
             to={nav.link}
             key={`nav-${index}`}
-            className={`sidebar__menu__item ${
-              activeIndex === index && "active"
+            className={`${classes.sidebar__menu__item} ${
+              activeIndex === index && classes.active
             }`}
           >
             <div className={classes.sidebar__menu__item__icon}>
               <Icon icon={nav.icon} />
             </div>
-            <div className="sidebar__menu__item__txt">{nav.text}</div>
+            <div className={classes.sidebar__menu__item__txt}>{nav.text}</div>
           </Link>
         ))}
+      </div>
+
+      <div className={[classes.sidebar__menu, classes.logout].join("")}>
+        <Link to="/" className={classes.sidebar__menu__item}>
+          <div className={classes.sidebar__menu__item__icon}>
+            <Icon icon="tabler:logout" />
+          </div>
+          <div className={classes.sidebar__menu__item__txt}>Logout</div>
+        </Link>
       </div>
     </div>
   );
