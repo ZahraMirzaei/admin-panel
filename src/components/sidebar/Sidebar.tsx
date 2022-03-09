7bd37adfededc67fbebef27ff399057ad5fb558a
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-09 19:34:05 +0330

    feat: :art: create sidebar with content

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { images } from "../../constants";
+import sidebarNav from "../../config/sidebarNav";
+import { Icon } from "@iconify/react";
 import classes from "./Sidebar.module.scss";
 
 function Sidebar() {
-  return <div className={`${classes.sideBar} ${classes.close}`}>Sidebar</div>;
+  const [activeIndex, setActiveIndex] = useState(0);
+  return (
+    <div className={classes.sidebar}>
+      <div className={classes.sidebar__logo}>
+        <img src={images.logo} alt="digikala" />
+      </div>
+      <div className={classes.sidebar__menu}>
+        {sidebarNav.map((nav, index) => (
+          <Link
+            to={nav.link}
+            key={`nav-${index}`}
+            className={`sidebar__menu__item ${
+              activeIndex === index && "active"
+            }`}
+          >
+            <div className={classes.sidebar__menu__item__icon}>
+              <Icon icon={nav.icon} />
+            </div>
+            <div className="sidebar__menu__item__txt">{nav.text}</div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
