9c475451de7a10d6ce8b68f5e86432e7ab821850
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-12 21:37:42 +0330

    feat: :art: add left top nav include menu icon and search component

--- src/components/topnav/TopNav.tsx
+++ src/components/topnav/TopNav.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Icon } from "@iconify/react";
+import SearchBox from "./searchBox/SearchBox";
+import TopNavRightBox from "./rightBox/TopNavRightBox";
+
+import classes from "./TopNav.module.scss";
 
 function TopNav() {
-  return <div>TopNav</div>;
+  return (
+    <div className={classes.topNav}>
+      <div className={classes.topNav_left}>
+        <Icon icon="ci:menu-alt-03" color="#424750" width="24" />
+        <SearchBox />
+      </div>
+    </div>
+  );
 }
 
 export default TopNav;
