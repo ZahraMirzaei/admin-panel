ec1b7690d2b3667928f55bed09b1205bdc669b5a
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-13 21:47:52 +0330

    feat: :lipstick: style theme styles and onClick

--- src/components/topnav/rightBox/themeBox/ThemeBox.tsx
+++ src/components/topnav/rightBox/themeBox/ThemeBox.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./ThemeBox.module.scss";
 
 function ThemeBox() {
+  const [theme, setTheme] = useState("light");
+
+  const themeChangeHandler = () => {
+    setTheme((prev) => {
+      if (prev === "light") {
+        return "dark";
+      } else {
+        return "light";
+      }
+    });
+  };
   return (
-    <div className={classes.themeBox}>
-      <div className={classes.toggle}></div>
+    <div className={classes.themeBox} onClick={themeChangeHandler}>
+      <div
+        className={`${classes.toggle} ${
+          theme === "dark" ? classes.darkMode : ""
+        }`}
+      ></div>
     </div>
   );
 }
