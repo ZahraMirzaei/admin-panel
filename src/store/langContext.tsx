547d2f6af611bbc8304ecea4f610e0aaf61a803f
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-15 11:58:57 +0330

    fix: :bug: language file in local storage

--- src/store/langContext.tsx
+++ src/store/langContext.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useLocalStorage } from "usehooks-ts";
 
 type langContextObj = {
@@ -7,7 +7,7 @@ type langContextObj = {
 };
 
 const LangContext = React.createContext<langContextObj>({
-  lang: "en",
+  lang: "",
   toggleLanguage: () => {},
 });
 
