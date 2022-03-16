35d1cd3e16f7706e0e9c9918e8696634c20ec4c0
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-16 21:18:25 +0330

    feat: :sparkles: add chart to dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import Summary from "../components/summary/Summary";
+import SaleChart from "../components/chart/Chart";
+
 function Dashboard() {
   const { t } = useTranslation();
   return (
     <section>
       <h2 className="title">{t("dashboard")}</h2>
       <Summary />
+      <SaleChart />
     </section>
   );
 }
