b92e8e5b243a3a0dde7dba43994ade6730cc2dd4
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-20 12:17:17 +0330

    refactor: :recycle: add table component to dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import Summary from "../components/summary/Summary";
 import SaleChart from "../components/chart/Chart";
+import Table from "../components/tables/Table";
 
 function Dashboard() {
   const { t } = useTranslation();
@@ -10,6 +11,7 @@ function Dashboard() {
       <h2 className="title">{t("dashboard")}</h2>
       <Summary />
       <SaleChart />
+      <Table />
     </section>
   );
 }
