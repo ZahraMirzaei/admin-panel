b3939ac157ad30339629a2c2e4fc3bd98b915956
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-20 12:08:46 +0330

    refactor: :sparkles: add customTable component

--- src/components/tables/customTable/CustomTable.tsx
+++ src/components/tables/customTable/CustomTable.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { Itable } from "../../../interfaces/Itable";
+import Card from "../../UI/card/Card";
+import { useTranslation } from "react-i18next";
 import classes from "./CustomTable.module.scss";
 
 const CustomTable: React.FC<Itable> = (props) => {
+  const { t } = useTranslation();
   const initDataShow =
     props.limit && props.bodyData
       ? props.bodyData.slice(0, Number(props.limit))
@@ -11,31 +14,27 @@ const CustomTable: React.FC<Itable> = (props) => {
   const [dataShow, setDataShow] = useState(initDataShow);
 
   return (
-    <div>
-      <div className={classes.table__wrapper}>
-        <table>
-          {props.headData ? (
-            <thead>
-              <tr>
-                {props.headData.map((item, index) => {
-                  <th key={index}>{item}</th>;
-                })}
-              </tr>
-            </thead>
-          ) : null}
-          {props.bodyData ? (
-            <tbody>
-              {dataShow.map((item, index) => (
-                <tr key={index}>
-                  {Object.values(item).map((i, idx) => (
-                    <td key={idx}>{i}</td>
+    <div className={classes.container}>
+      <Card>
+        <div className={classes.table__wrapper}>
+          <table>
+            {props.headData ? (
+              <thead>
+                <tr>
+                  {props.headData.map((item, index) => (
+                    <th key={index}>{t(item)}</th>
                   ))}
                 </tr>
-              ))}
-            </tbody>
-          ) : null}
-        </table>
-      </div>
+              </thead>
+            ) : null}
+            {props.bodyData && props.renderBody ? (
+              <tbody>
+                {dataShow.map((item, index) => props.renderBody(item, index))}
+              </tbody>
+            ) : null}
+          </table>
+        </div>
+      </Card>
     </div>
   );
 };
