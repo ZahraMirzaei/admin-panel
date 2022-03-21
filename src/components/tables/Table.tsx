e1ac544e1d068a7a6786926506bcf5d9a8e7dd62
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-21 10:46:26 +0330

    refactor: :recycle: add another copy table for arrange tables and add wrapper for each table. and change styles based on .tsx file changes

--- src/components/tables/Table.tsx
+++ src/components/tables/Table.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useCallback} from "react";
 import { Link } from "react-router-dom";
 import classes from "./Table.module.scss";
 import { useTranslation } from "react-i18next";
@@ -7,52 +7,55 @@ import Badge from "../UI/badge/Badge";
 import data from "../../constants/data";
 import { TtopCustomers, TlatestTransactions } from "../../interfaces/Itable";
 
-const renderCusomerBody = (item: TtopCustomers, index: number) => (
-  <tr key={index}>
-    <td>{item.username}</td>
-    <td>{item.order}</td>
-    <td>{item.price}</td>
-  </tr>
-);
-const renderOrderBody = (item: TlatestTransactions, index: number) => (
-  <tr key={index}>
-    <td>{item.orderId}</td>
-    <td>{item.customer}</td>
-    <td>{item.totalPrice}</td>
-    <td>{item.date}</td>
-    <td>
-      <Badge content={item.status} />
-    </td>
-  </tr>
-);
 const Table = () => {
+  const renderCusomerBody = useCallback((item: TtopCustomers, index: number) => (
+    <tr key={index}>
+      <td>{item.username}</td>
+      <td>{item.order}</td>
+      <td>{item.price}</td>
+    </tr>
+  ),[]);
+  const renderOrderBody = useCallback((item: TlatestTransactions, index: number) => (
+    <tr key={index}>
+      <td>{item.orderId}</td>
+      <td>{item.customer}</td>
+      <td>{item.totalPrice}</td>
+      <td>{item.date}</td>
+      <td>
+        <Badge content={item.status} />
+      </td>
+    </tr>
+  ),[]);
   const { t } = useTranslation();
-  return (
-    <section className={classes.table}>
-      {/* <div className={classes.table__top__customers}> */}
-      <div className={classes.table__title}>
-        <p className="subTitle">{t("topCustomers")}</p>
-        <Link to="/">{t("viewAll")}</Link>
-      </div>
-      <CustomTable
-        headData={data.topCustomers.head}
-        bodyData={data.topCustomers.body}
-        renderBody={renderCusomerBody}
-      />
-      {/* </div> */}
-      {/* <div className={classes.table__top__customers}> */}
-      {/* <div className={classes.table__title}>
-        <p className="subTitle">{t("topCustomers")}</p>
-        <Link to="/">{t("viewAll")}</Link>
-      </div>
-      <CustomTable
-        headData={data.topCustomers.head}
-        bodyData={data.topCustomers.body}
-        renderBody={renderCusomerBody}
-      /> */}
-      {/* </div> */}
-    </section>
-  );
+  if ("username" in )
+    return (
+      <section className={classes.table}>
+        <div className={classes.table__top__customers}>
+          <div className={classes.table__title}>
+            <p className="subTitle">{t("topCustomers")}</p>
+            <Link to="/">{t("viewAll")}</Link>
+          </div>
+          {
+            <CustomTable
+              headData={data.topCustomers.head}
+              bodyData={data.topCustomers.body}
+              renderBody={renderCusomerBody}
+            />
+          }
+        </div>
+        <div className={classes.table__top__customers}>
+          <div className={classes.table__title}>
+            <p className="subTitle">{t("topCustomers")}</p>
+            <Link to="/">{t("viewAll")}</Link>
+          </div>
+          <CustomTable
+            headData={data.topCustomers.head}
+            bodyData={data.topCustomers.body}
+            renderBody={renderCusomerBody}
+          />
+        </div>
+      </section>
+    );
 };
 
 export default Table;
