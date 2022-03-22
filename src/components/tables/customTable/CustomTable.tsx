e228c245d80f5a3260bc1779963d1ed0eb1fb161
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-22 23:36:45 +0430

    feat: :art: add type guarde for customers table

--- src/components/tables/customTable/CustomTable.tsx
+++ src/components/tables/customTable/CustomTable.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Itable, complex } from "../../../interfaces/Itable";
 import Card from "../../UI/card/Card";
 import Badge from "../../UI/badge/Badge";
 import { useTranslation } from "react-i18next";
+import { Icon } from "@iconify/react";
 import classes from "./CustomTable.module.scss";
 
 const CustomTable: React.FC<Itable> = (props) => {
@@ -24,6 +26,31 @@ const CustomTable: React.FC<Itable> = (props) => {
           <td>{item.date}</td>
           <td>
             <Badge content={item.status} />
+          </td>
+        </tr>
+      );
+    } else if ("email" in item) {
+      return (
+        <tr key={index}>
+          <td>{item.ID}</td>
+          <td>{item.userName}</td>
+          <td className="ltr">{item.email}</td>
+          <td className="ltr">{item.phoneNumber}</td>
+          <td>{item.totalOrders}</td>
+          <td>{item.totalSpend}</td>
+          <td>{item.location}</td>
+          <td className={classes.actions}>
+            <Icon icon="charm:menu-kebab" />
+            <div className={classes.actions__box}>
+              <div className={classes.actions__edit}>
+                <Link to="/">
+                  <Icon icon="fluent:edit-16-regular" width="24" />
+                </Link>
+              </div>
+              <div className={classes.actions__delete}>
+                <Icon icon="fluent:delete-24-regular" width="24" />
+              </div>
+            </div>
           </td>
         </tr>
       );
