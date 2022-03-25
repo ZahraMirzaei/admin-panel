815146003f016cd34e84181f6ab7e64fd91d8fb9
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-25 09:47:53 +0430

    feat: :sparkles: add pagination to custom table

--- src/components/tables/customTable/CustomTable.tsx
+++ src/components/tables/customTable/CustomTable.tsx
@@ -14,7 +14,9 @@ const CustomTable: React.FC<Itable> = (props) => {
     setShowModal((prev) => !prev);
   }
   function tableBody(item: complex, index: number) {
+    /* type guard (in typescript) */
     if ("username" in item) {
+      //for implementing top customers
       return (
         <tr key={index}>
           <td>{item.username}</td>
@@ -23,6 +25,7 @@ const CustomTable: React.FC<Itable> = (props) => {
         </tr>
       );
     } else if ("orderId" in item) {
+      //for implementing latest transactions
       return (
         <tr key={index}>
           <td>{item.orderId}</td>
@@ -35,6 +38,7 @@ const CustomTable: React.FC<Itable> = (props) => {
         </tr>
       );
     } else if ("email" in item) {
+      //for implementing customers table
       return (
         <tr key={index}>
           <td>{item.ID}</td>
@@ -65,10 +69,39 @@ const CustomTable: React.FC<Itable> = (props) => {
     }
   }
 
+  const initDataShow =
+    props.limit && props.bodyData
+      ? props.bodyData.slice(0, Number(props.limit))
+      : props.bodyData;
+
+  const [dataShow, setDataShow] = useState(initDataShow);
+
+  let pages = 1;
+
+  let range: number[] = [];
+
+  if (props.limit !== undefined) {
+    let page = Math.floor(props.bodyData.length / Number(props.limit));
+    pages = props.bodyData.length % Number(props.limit) === 0 ? page : page + 1;
+    range = [...Array(pages).keys()];
+  }
+
+  const [currPage, setCurrPage] = useState(0);
+
+  const selectPage = (page: number) => {
+    const start = Number(props.limit) * page;
+    const end = start + Number(props.limit);
+
+    setDataShow(props.bodyData.slice(start, end));
+
+    setCurrPage(page);
+  };
+
   const { t } = useTranslation();
 
   return (
     <>
+      {/* modal for delete customer and product case*/}
       {showModal ? (
         <Modal
           title={t("deleteCustomer")}
@@ -76,23 +109,44 @@ const CustomTable: React.FC<Itable> = (props) => {
           onConfirm={showModalHandler}
         />
       ) : null}
+
       <div className={classes.container}>
         <Card>
-          <div className={classes.table__wrapper}>
-            <table>
-              {props.headData ? (
-                <thead>
-                  <tr>
-                    {props.headData.map((item, index) => (
-                      <th key={index}>{t(item)}</th>
-                    ))}
-                  </tr>
-                </thead>
-              ) : null}
-              <tbody>
-                {props.bodyData.map((item, index) => tableBody(item, index))}
-              </tbody>
-            </table>
+          <div className={classes.wrapper}>
+            <div className={classes.table__wrapper}>
+              <table
+                className={props.limit ? classes.largeTable : classes.table}
+              >
+                {props.headData ? (
+                  <thead>
+                    <tr>
+                      {props.headData.map((item, index) => (
+                        <th key={index}>{t(item)}</th>
+                      ))}
+                    </tr>
+                  </thead>
+                ) : null}
+                <tbody>
+                  {dataShow.map((item, index) => tableBody(item, index))}
+                </tbody>
+              </table>
+            </div>
+
+            {pages > 1 ? (
+              <div className={classes.table__pagination}>
+                {range.map((item, index) => (
+                  <div
+                    key={index}
+                    className={`${classes.table__pagination_item} ${
+                      currPage === index ? classes.active : ""
+                    }`}
+                    onClick={() => selectPage(index)}
+                  >
+                    {item + 1}
+                  </div>
+                ))}
+              </div>
+            ) : null}
           </div>
         </Card>
       </div>
