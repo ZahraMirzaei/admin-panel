a9d1e0ccc1f55f9f0bb10888f9052902b62fd8a0
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-25 21:47:20 +0430

    feat: :sparkles: add Dropdown component

---
+++ src/components/UI/dropdown/Dropdown.tsx
@@ -0,0 +1,39 @@
+import React, { useRef, useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import classes from "./Dropdown.module.scss";
+
+type TOptionItem = {
+  label: string;
+  value: string;
+};
+
+interface IDropdown {
+  dropdownData: TOptionItem[];
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+const Dropdown: React.FC<IDropdown> = (props) => {
+  const selectRef = useRef<HTMLSelectElement | null>(null);
+
+  const { t } = useTranslation();
+
+  return (
+    <div className={` subTitle ${classes.form__control}`}>
+      <label htmlFor="category">{t("category")}</label>
+      <select
+        name="category"
+        className={classes.select}
+        id="category"
+        ref={selectRef}
+        onChange={props.onChange}
+      >
+        {props.dropdownData.map((item, index) => (
+          <option key={`${item.label + index}`} value={item.value}>
+            {t(`${item.label}`)}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Dropdown;
