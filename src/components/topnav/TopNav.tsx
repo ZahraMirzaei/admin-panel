dec39c19ebe4f307f3e948c0557234fc97bfeba2
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-27 20:41:20 +0430

    feat: :sparkles: add close icon and its actions to sidebar in tablet size and add sidebar__open class to body, if device width was less then 768px

--- src/components/topnav/TopNav.tsx
+++ src/components/topnav/TopNav.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { Icon } from "@iconify/react";
+import { useWindowSize } from "usehooks-ts";
 import SearchBox from "./searchBox/SearchBox";
 import TopNavRightBox from "./rightBox/TopNavRightBox";
 import SidebarContext from "../../store/sidebarContext";
@@ -8,15 +9,30 @@ import classes from "./TopNav.module.scss";
 
 function TopNav() {
   const sideOpenCtx = useContext(SidebarContext);
+  const { width } = useWindowSize();
 
-  function clickHandler() {
+  function openSidebarHandler() {
     sideOpenCtx.toggleSidebar();
+    if (width <= 768) document.body.classList.toggle("sidebar__open");
   }
+
   return (
     <div className={classes.topNav}>
       <div className={classes.topNav_left}>
-        <div className={classes.topNav_left_menu_icon} onClick={clickHandler}>
+        <div
+          className={classes.topNav_left_menu_icon}
+          onClick={openSidebarHandler}
+        >
           <Icon icon="ci:menu-alt-03" width="24" />
+        </div>
+        <div className={classes.topNav_left_menu} onClick={openSidebarHandler}>
+          <div className="topNav_left_menu_open">
+            <Icon icon="ci:menu-alt-03" width="24" />
+          </div>
+
+          <div className="topNav_left_menu_close">
+            <Icon icon="eva:close-fill" width="24" />
+          </div>
         </div>
         <div className={classes.search_desktop_wrapper}>
           <SearchBox />
