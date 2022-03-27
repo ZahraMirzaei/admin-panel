95bbdb9502b6213bf5eae8222fd0ad13de142533
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-27 17:27:28 +0430

    feat: :art: add login page svg to assets

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,6 +1,8 @@
 const images = {
   logo: require("../assets/images/digikalaLogo.png"),
   avt: require("../assets/images/avatar.jpg"),
+  logKey: require("../assets/images/Reset password-pana.svg"),
+  dashboard: require("../assets/images/Revenue-cuate.svg"),
 };
 
 export default images;
