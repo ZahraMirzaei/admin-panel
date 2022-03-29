632522d7478732c0baaa4324cbcf8eebfed09c93
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-29 10:50:26 +0430

    feat: :sparkles: add notFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,43 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { t } from "i18next";
 import React from "react";
+import Button from "../components/UI/button/Button";
+import { useTranslation } from "react-i18next";
+import langContextObj from "../store/langContext";
 
 function NotFound() {
-  return <div>NotFound</div>;
+  const { t } = useTranslation();
+  const langCtx = useContext(langContextObj);
+  return (
+    <div
+      className={`${
+        langCtx.lang === "en"
+          ? "notFound__container"
+          : "notFound__container_rtl"
+      }`}
+    >
+      <h2
+        className={` notFound__title ${
+          langCtx.lang === "fa" ? "notFound__title_rtl" : "notFound__title_ltr"
+        }`}
+      >
+        {t("notFoundMsg")}
+      </h2>
+      <Link to="/">
+        <Button>{t("backToHome")}</Button>
+      </Link>
+      <div className="notFound__img">
+        <img
+          src={
+            require("../assets/images/Oops 404 Error with a broken robot-cuate.svg")
+              .default
+          }
+          alt="404 page"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
