250be9f9402dd6a2082878df24d3e699495d8a55
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-29 14:48:32 +0430

    refactor: :sparkles: add lazy loading to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,51 @@
-import { useContext } from "react";
+import React, { useContext, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Customers from "./pages/Customers";
-import CustomerEdit from "./pages/CustomerEdit";
-import Products from "./pages/Products";
-import ProductEdit from "./pages/ProductEdit";
-import NotFound from "./pages/NotFound";
-import BlankPage from "./pages/BlankPage";
-import Login from "./pages/Login";
+// import Dashboard from "./pages/Dashboard";
+// import Customers from "./pages/Customers";
+// import CustomerEdit from "./pages/CustomerEdit";
+// import Products from "./pages/Products";
+// import ProductEdit from "./pages/ProductEdit";
+// import NotFound from "./pages/NotFound";
+// import BlankPage from "./pages/BlankPage";
+// import Login from "./pages/Login";
 import MainLayout from "./layout/MainLayout";
 import LoginContext from "./store/loginContext";
-
+import LoadingSpinner from "./components/UI/loadingSpinner/LoadingSpinner";
 import "./scss/App.scss";
+
+const Dashboard = React.lazy(() => import("./pages/Dashboard"));
+const Customers = React.lazy(() => import("./pages/Customers"));
+const CustomerEdit = React.lazy(() => import("./pages/CustomerEdit"));
+const Products = React.lazy(() => import("./pages/Products"));
+const ProductEdit = React.lazy(() => import("./pages/ProductEdit"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const BlankPage = React.lazy(() => import("./pages/BlankPage"));
+const Login = React.lazy(() => import("./pages/Login"));
+
 function App() {
   const loginCtx = useContext(LoginContext);
   return (
     <BrowserRouter>
-      <Routes>
-        {!loginCtx.isLogin ? (
-          <Route path="/" element={<Login />} />
-        ) : (
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="/customers" element={<Customers />} />
-            <Route path="/customers/:customerId" element={<CustomerEdit />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/products/:productId" element={<ProductEdit />} />
-            <Route path="/orders" element={<BlankPage />} />
-            <Route path="/analytics" element={<BlankPage />} />
-            <Route path="/discount" element={<BlankPage />} />
-            <Route path="/inventory" element={<BlankPage />} />
-          </Route>
-        )}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Routes>
+          {!loginCtx.isLogin ? (
+            <Route path="/" element={<Login />} />
+          ) : (
+            <Route path="/" element={<MainLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="/customers" element={<Customers />} />
+              <Route path="/customers/:customerId" element={<CustomerEdit />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/products/:productId" element={<ProductEdit />} />
+              <Route path="/orders" element={<BlankPage />} />
+              <Route path="/analytics" element={<BlankPage />} />
+              <Route path="/discount" element={<BlankPage />} />
+              <Route path="/inventory" element={<BlankPage />} />
+            </Route>
+          )}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
