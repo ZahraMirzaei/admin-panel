f8f3d4ce9ea60bef9d45f34605ad73b18a44458a
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-03-29 10:48:23 +0430

    feat: :bento: add image for not found page

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -3,6 +3,7 @@ const images = {
   avt: require("../assets/images/avatar.jpg"),
   logKey: require("../assets/images/Reset password-pana.svg"),
   dashboard: require("../assets/images/Revenue-cuate.svg"),
+  notFound: require("../assets/images/Oops 404 Error with a broken robot-cuate.svg"),
 };
 
 export default images;
