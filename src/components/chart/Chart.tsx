d0d641d1830e4b33ad2eb1b9942cc2610ecc0414
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-04-08 23:17:01 +0430

    feat: :sparkles: add Bar charts in chart component

--- src/components/chart/Chart.tsx
+++ src/components/chart/Chart.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
+import { faker } from "@faker-js/faker";
+
 import { useTranslation } from "react-i18next";
 import LineChart from "./LineChart";
+import BarChart from "./BarChart";
 import classes from "./Chart.module.scss";
 import data from "../../constants/data";
 import Card from "../UI/card/Card";
 
 const SaleChart = () => {
   const { t } = useTranslation();
-
+  const labels = data.revenueByMonths.labels.map((month) => t(month));
   const [userData, setUserData] = useState({
-    labels: data.revenueByMonths.labels.map((month) => t(month)),
+    labels,
     datasets: [
       {
         label: t("summaryOfSale"),
@@ -19,14 +22,57 @@ const SaleChart = () => {
       },
     ],
   });
+
+  const [orderData, setOrderData] = useState({
+    labels,
+    datasets: [
+      {
+        label: t("summaryOfOrders"),
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  });
+
+  const [revenueData, setRevenueData] = useState({
+    labels,
+    datasets: [
+      {
+        label: t("summaryOfRevenue"),
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  });
+
   return (
     <section className={classes.chart}>
       <p className="subTitle">{t("quickAnalysis")}</p>
-      <Card>
-        <div className={classes.chart__wrapper}>
-          <LineChart chartData={userData} />
+      <div className={classes.charts__container}>
+        <div className={classes.charts__wrapper}>
+          <Card>
+            <div className={classes.chart__wrapper}>
+              <BarChart
+                chartData={orderData}
+                chartTitle={`${t("summaryOfOrders")}`}
+              />
+            </div>
+          </Card>
+          <Card>
+            <div className={classes.chart__wrapper}>
+              <BarChart
+                chartData={revenueData}
+                chartTitle={`${t("summaryOfRevenue")}`}
+              />
+            </div>
+          </Card>
         </div>
-      </Card>
+        <Card>
+          <div className={classes.chart__wrapper}>
+            <LineChart chartData={userData} />
+          </div>
+        </Card>
+      </div>
     </section>
   );
 };
