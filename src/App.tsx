6ba4e15ceb4a33392ba1305647c2e3c0b2a01f84
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-04-10 23:50:03 +0430

    refactor: :poop: remove login route for now.

--- src/App.tsx
+++ src/App.tsx
@@ -28,21 +28,21 @@ function App() {
     <BrowserRouter>
       <Suspense fallback={<LoadingSpinner />}>
         <Routes>
-          {!loginCtx.isLogin ? (
+          {/* {!loginCtx.isLogin ? (
             <Route path="/" element={<Login />} />
-          ) : (
-            <Route path="/" element={<MainLayout />}>
-              <Route index element={<Dashboard />} />
-              <Route path="/customers" element={<Customers />} />
-              <Route path="/customers/:customerId" element={<CustomerEdit />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/products/:productId" element={<ProductEdit />} />
-              <Route path="/orders" element={<BlankPage />} />
-              <Route path="/analytics" element={<BlankPage />} />
-              <Route path="/discount" element={<BlankPage />} />
-              <Route path="/inventory" element={<BlankPage />} />
-            </Route>
-          )}
+          ) : ( */}
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/customers/:customerId" element={<CustomerEdit />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:productId" element={<ProductEdit />} />
+            <Route path="/orders" element={<BlankPage />} />
+            <Route path="/analytics" element={<BlankPage />} />
+            <Route path="/discount" element={<BlankPage />} />
+            <Route path="/inventory" element={<BlankPage />} />
+          </Route>
+          {/* )} */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
