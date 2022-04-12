34fd89362732110b1b8e4e53001af98085f872f5
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-04-12 06:59:13 +0430

    docs: :coffin: remove commented code from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Dashboard from "./pages/Dashboard";
-// import Customers from "./pages/Customers";
-// import CustomerEdit from "./pages/CustomerEdit";
-// import Products from "./pages/Products";
-// import ProductEdit from "./pages/ProductEdit";
-// import NotFound from "./pages/NotFound";
-// import BlankPage from "./pages/BlankPage";
-// import Login from "./pages/Login";
+
 import MainLayout from "./layout/MainLayout";
 import AuthLayout from "./layout/AuthLayout";
-// import LoginContext from "./store/loginContext";
 import LoadingSpinner from "./components/UI/loadingSpinner/LoadingSpinner";
 import "./scss/App.scss";
 
@@ -22,43 +14,12 @@ const ProductEdit = React.lazy(() => import("./pages/ProductEdit"));
 const NotFound = React.lazy(() => import("./pages/NotFound"));
 const BlankPage = React.lazy(() => import("./pages/BlankPage"));
 const Login = React.lazy(() => import("./pages/Login"));
-// const ProtectedRoute = React.lazy(() => import("./route/ProtectedRoute"));
 
 function App() {
-  // const loginCtx = useContext(LoginContext);
   return (
     <BrowserRouter>
       <Suspense fallback={<LoadingSpinner />}>
         <Routes>
-          {/* {!loginCtx.isLogin ? (
-            <Route path="/" element={<Login />} />
-          ) : (
-            <Route path="/" element={<MainLayout />}>
-              <Route index element={<Dashboard />} />
-              <Route path="/customers" element={<Customers />} />
-              <Route path="/customers/:customerId" element={<CustomerEdit />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/products/:productId" element={<ProductEdit />} />
-              <Route path="/orders" element={<BlankPage />} />
-              <Route path="/analytics" element={<BlankPage />} />
-              <Route path="/discount" element={<BlankPage />} />
-              <Route path="/inventory" element={<BlankPage />} />
-            </Route>
-          )} */}
-          {/* <Route path="/" element={<Login />} />
-          <Route element={<ProtectedRoute />}> */}
-          {/* <Route path="/" element={<MainLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="/customers" element={<Customers />} />
-            <Route path="/customers/:customerId" element={<CustomerEdit />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/products/:productId" element={<ProductEdit />} />
-            <Route path="/orders" element={<BlankPage />} />
-            <Route path="/analytics" element={<BlankPage />} />
-            <Route path="/discount" element={<BlankPage />} />
-            <Route path="/inventory" element={<BlankPage />} />
-          </Route> */}
-          {/* </Route> */}
           <Route element={<AuthLayout />}>
             <Route path="/" element={<MainLayout />}>
               <Route index element={<Dashboard />} />
